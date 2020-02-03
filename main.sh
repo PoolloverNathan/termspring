21ad962d12173b550d46ac055a8555d86727268d
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-02-03 13:13:52 -0500

    Create the main looper

--- main.sh
+++ main.sh
@@ -1,8 +1,15 @@
+SCREEN=login
+FOCUS=username
+
 winch () {
   LINES=$(tput lines)
   COLS=$(tput cols)
   (( lines2 = LINES / 2 ))
   (( cols2 = COLS / 2 ))
 }
+
+
+
+while true; do mainloop; done
 
 trap winch WINCH
